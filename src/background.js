74f39d1a95a328ccbb4d8703b63db9ea5b9b516a
mark <mark.kurman@gmail.com>
2017-11-10 19:59:44 +0200

    background script saves basic dict to local storage after installation the content script sends a message to the background asking for the dict after the content script gets the response it runs the catagorixation the loging was broken down into functions

---
+++ src/background.js
@@ -0,0 +1,460 @@
+ 
+var orig_categorized_labels = {
+	"values": [{
+		"label": "סופר פארם פקר כ\"ס 15",
+		"category": "pharm"
+	}, {
+		"label": "רשת משחקיות פעלטון ב",
+		"category": "recreation.kids.crafts"
+	}, {
+		"label": "אלונית מושב נוה ירק-",
+		"category": "groceries.alonit"
+	}, {
+		"label": "תינוקי",
+		"category": "recreation.kids.games"
+	}, {
+		"label": "קפה ברוריה בר קפה",
+		"category": "recreation.coffee"
+	}, {
+		"label": "מקס סטוק כפר סבא",
+		"category": "home.improvement"
+	}, {
+		"label": "T.REX",
+		"category": "recreation.kids.games"
+	}, {
+		"label": "הפנינג קניון מרגליות",
+		"category": "recreation.kids.games"
+	}, {
+		"label": "דומינו'ס פיצה",
+		"category": "restaurants.takeout"
+	}, {
+		"label": "לחם ארטיזן הוד  השרו",
+		"category": "groceries"
+	}, {
+		"label": "BIGA כפר סבא הירוקה",
+		"category": "recreation.coffee"
+	}, {
+		"label": "קפה גרג קניון מרגלית",
+		"category": "recreation.coffee"
+	}, {
+		"label": "העברה ב BIT בנה\"פ",
+		"category": "bit"
+	}, {
+		"label": "חניון בית רובינשטיין",
+		"category": "parking"
+	}, {
+		"label": "יוליה נמל תל-אביב בע",
+		"category": "restaurants"
+	}, {
+		"label": "אילנס אוטרייב",
+		"category": "recreation.coffee"
+	}, {
+		"label": "פקק קפה האסקי",
+		"category": "recreation.coffee"
+	}, {
+		"label": "משק רוזנטל",
+		"category": "groceries"
+	}, {
+		"label": "חניון נמל תל אביב",
+		"category": "parking"
+	}, {
+		"label": "קפה קפה רננים",
+		"category": "recreation.coffee"
+	}, {
+		"label": "בוקריה",
+		"category": "restaurants"
+	}, {
+		"label": "חניון יכין",
+		"category": "parking"
+	}, {
+		"label": "סופר אלונית -נווה יר",
+		"category": "groceries.alonit"
+	}, {
+		"label": "וניליה הוד השרון",
+		"category": "recreation.coffee"
+	}, {
+		"label": "מנגו גינדי ת\"א",
+		"category": "clothes"
+	}, {
+		"label": "ק.ד עבוד קים ביוטי ס",
+		"category": "beauty"
+	}, {
+		"label": "פלוס לגננת",
+		"category": "recreation.kids.crafts"
+	}, {
+		"label": "ניופארם שרונים",
+		"category": "pharm"
+	}, {
+		"label": "קאב קם בע\"מ",
+		"category": "restaurants"
+	}, {
+		"label": "מס הכנסה עצמאים וחבר",
+		"category": "office.irs"
+	}, {
+		"label": "גביית מע\"מ אינטרנט -",
+		"category": "office.vat"
+	}, {
+		"label": "ביי-מי שוברי מתנה",
+		"category": "recreation.kids.games"
+	}, {
+		"label": "זארה כפר סבא",
+		"category": "clothes"
+	}, {
+		"label": "ארקפה ג'י",
+		"category": "recreation.coffee"
+	}, {
+		"label": "מאצ' ריטייל בע\"מ HאM",
+		"category": "clothes"
+	}, {
+		"label": "דור אלון - נווה ירק-",
+		"category": "car.gas"
+	}, {
+		"label": "קפה לנדוור אושירה כ\"",
+		"category": "recreation.coffee"
+	}, {
+		"label": "סטימצקי עזריאלי הוד",
+		"category": "recreation.books"
+	}, {
+		"label": "לילי ותום",
+		"category": "clothes"
+	}, {
+		"label": "סינטה בר בע\"מ",
+		"category": "restaurants"
+	}, {
+		"label": "ארומה ק. מרגלית  השר",
+		"category": "recreation.coffee"
+	}, {
+		"label": "ארומה כפר סבא",
+		"category": "recreation.coffee"
+	}, {
+		"label": "סופר פארם הוד השרון",
+		"category": "pharm"
+	}, {
+		"label": "פלוס לגננת בע\"מ",
+		"category": "recreation.kids.crafts"
+	}, {
+		"label": "באליגם",
+		"category": "other"
+	}, {
+		"label": "ZOZOBRA-גמא",
+		"category": "restaurants"
+	}, {
+		"label": "כל בו חצי חינם הוד ה",
+		"category": "groceries"
+	}, {
+		"label": "החומוס של ג'ינג'י",
+		"category": "restaurants"
+	}, {
+		"label": "מסעדת שגב אקספרס",
+		"category": "restaurants"
+	}, {
+		"label": "דרום השרון גבייה",
+		"category": "home.arnona"
+	}, {
+		"label": "סוזט",
+		"category": "restaurants"
+	}, {
+		"label": "אלונית נוה ירק",
+		"category": "groceries.alonit"
+	}, {
+		"label": "אלונית מושב נווה ירק",
+		"category": "groceries.alonit"
+	}, {
+		"label": "ארומה רמת החייל",
+		"category": "recreation.coffee"
+	}, {
+		"label": "רמי לוי אינטרנט",
+		"category": "groceries"
+	}, {
+		"label": "מסעדת NONO",
+		"category": "restaurants"
+	}, {
+		"label": "בית תאילנדי בע\"מ",
+		"category": "restaurants"
+	}, {
+		"label": "דור אלון מושב נוה ירק",
+		"category": "car.gas"
+	}, {
+		"label": "אגנס בר",
+		"category": "restaurants"
+	}, {
+		"label": "מרכז שירות לרכב גיל",
+		"category": "car.service"
+	}, {
+		"label": "ארקפה רמת החייל",
+		"category": "recreation.coffee"
+	}, {
+		"label": "קסטרו",
+		"category": "clothes"
+	}, {
+		"label": "לחמים מאפייה ואספרסו בר",
+		"category": "recreation.coffee"
+	}, {
+		"label": "CATIMINI",
+		"category": "clothes"
+	}, {
+		"label": "כלבו חצי חינם הוד השרון",
+		"category": "groceries"
+	}, {
+		"label": "שוק חצי חינם",
+		"category": "groceries"
+	}, {
+		"label": "A TO Z LOFT-צמרת",
+		"category": "beauty"
+	}, {
+		"label": "טפאו",
+		"category": "restaurants"
+	}, {
+		"label": "קניון העיר תל אביב",
+		"category": "parking"
+	}, {
+		"label": "משקאות הטורקי",
+		"category": "groceries"
+	}, {
+		"label": "מ.נ.מ הכל לתעשיה בע\"מ",
+		"category": "home.improvement"
+	}, {
+		"label": "כביש 6",
+		"category": "rout6"
+	}, {
+		"label": "דלאל",
+		"category": "restaurants"
+	}, {
+		"label": "לנדוור רמת החייל",
+		"category": "recreation.coffee"
+	}, {
+		"label": "רחיצת ספידו",
+		"category": "car.wash"
+	}, {
+		"label": "ח-ן שרות סלקום",
+		"category": "recreation.tv"
+	}, {
+		"label": "ג'רמיה",
+		"category": "restaurants"
+	}, {
+		"label": "AMAZON SERVICES-KINDLE 866-321-8851",
+		"category": "recreation.books"
+	}, {
+		"label": "כספומט אלונית נ מושב נוו",
+		"category": "cash"
+	}, {
+		"label": "ITUNES.COM/BILL        ITUNES.COM",
+		"category": "recreation.other"
+	}, {
+		"label": "WWW.ALIEXPRESS.COM     LONDON",
+		"category": "aliexpress"
+	}, {
+		"label": "NETFLIX.COM            AMSTERDAM",
+		"category": "recreation.tv"
+	}, {
+		"label": "קסטרו כפר סבא - אושי",
+		"category": "clothes"
+	}, {
+		"label": "רכבת סגולה - פתח תקו",
+		"category": "car.alternative"
+	}, {
+		"label": "איקאה נתניה",
+		"category": "home.improvement"
+	}, {
+		"label": "הום סנטר כפר סבא",
+		"category": "home.improvement"
+	}, {
+		"label": "איקאה מסעדות ראשל\"צ",
+		"category": "recreation.coffee"
+	}, {
+		"label": "שגב קונספט כפר סבא",
+		"category": "restaurants"
+	}, {
+		"label": "הוניגמן T N T",
+		"category": "clothes"
+	}, {
+		"label": "איקאה-ריהוט ועיצוב הבית",
+		"category": "home.improvement"
+	}, {
+		"label": "יס פלאנט זכרון יעקב -קופו",
+		"category": "recreation.other"
+	}, {
+		"label": "יס פלאנט זכרון יעקב - מזנ",
+		"category": "recreation.other"
+	}, {
+		"label": "קרוסטה פיצה וחברים",
+		"category": "restaurants"
+	}, {
+		"label": "קלאב פארם בע\"מ",
+		"category": "pharm"
+	}, {
+		"label": "מקדונלד'ס אלון ושרון",
+		"category": "restaurants"
+	}, {
+		"label": "ישראדאנס רשת חנויות",
+		"category": "clothes"
+	}, {
+		"label": "PET POINT",
+		"category": "kiwi"
+	}, {
+		"label": "יער האיילים",
+		"category": "vacation"
+	}, {
+		"label": "יער האיילים",
+		"category": "vacation"
+	}, {
+		"label": "LAKA",
+		"category": "beauty"
+	}, {
+		"label": "לחייל ולטייל",
+		"category": "clothes"
+	}, {
+		"label": "לחייל ולטייל",
+		"category": "vacation"
+	}, {
+		"label": "מסעדת נידאל",
+		"category": "restaurants"
+	}, {
+		"label": "ארומה גן הצפון",
+		"category": "recreation.coffee"
+	}, {
+		"label": "מיטשוס בעמ",
+		"category": "restaurants"
+	}, {
+		"label": "טן רומת אל הייב",
+		"category": "car.gas"
+	}, {
+		"label": "הסטודיו של יעל",
+		"category": "recreation.hoog"
+	}, {
+		"label": "בית מרקחת סביון עין",
+		"category": "pharm"
+	}, {
+		"label": "ספרינט מוטורוס בע\"מ -  שע",
+		"category": "car.gas"
+	}, {
+		"label": "גולף קידס און ליין-ג",
+		"category": "clothes"
+	}, {
+		"label": "B בזק הוראת קבע",
+		"category": "home.internet"
+	}, {
+		"label": "אר קפה",
+		"category": "recreation.coffee"
+	}, {
+		"label": "ארקפה הבימה",
+		"category": "recreation.coffee"
+	}, {
+		"label": "הודיס בעמ",
+		"category": "clothes"
+	}, {
+		"label": "חברת חשמל מחוז דרום",
+		"category": "home.electric"
+	}, {
+		"label": "סופר-פארם  - קניון V",
+		"category": "pharm"
+	}, {
+		"label": "רולדין הוד השרון",
+		"category": "recreation.coffee"
+	}, {
+		"label": "מחסני תאורה כפר סבא-",
+		"category": "home.improvement"
+	}, {
+		"label": "JAPANIKA-אם הדרך",
+		"category": "restaurants"
+	}, {
+		"label": "מנהרות הכרמל-שער אגרה",
+		"category": "rout6"
+	}, {
+		"label": "סופרפארם רמת חייל",
+		"category": "pharm"
+	}, {
+		"label": "רכבת סבידור תל אביב",
+		"category": "car.alternative"
+	}, {
+		"label": "קפה מתתיהו-גמא",
+		"category": "recreation.coffee"
+	}, {
+		"label": "ג.י.ל לבובסקי בע''מ",
+		"category": "recreation.coffee"
+	}, {
+		"label": "רכבת הוד השרון - כפר",
+		"category": "car.alternative"
+	}, {
+		"label": "לנדוור הוד השרון",
+		"category": "recreation.coffee"
+	}, {
+		"label": "תמריק-קפה מעדנייה",
+		"category": "recreation.coffee"
+	}]
+};
+
+
+var colors = { 
+        "aliexpress": "DeepSkyBlue", 
+        "beauty": "DarkTurquoise", 
+        "bit": "LightGray", 
+        "car.gas": "RosyBrown", 
+        "car.service": "RosyBrown", 
+        "car.wash": "RosyBrown", 
+        "car.alternative": "RosyBrown", 
+        "cash": "CadetBlue", 
+        "clothes": "Coral", 
+        "groceries": "Aqua", 
+        "groceries.alonit": "Aqua", 
+        "home.arnona": "DarkSalmon", 
+        "home.improvement": "DarkSalmon", 
+        "home.electric": "DarkSalmon", 
+        "home.internet": "DarkSalmon", 
+        "kiwi": "Coral",
+        "office.irs": "GreenYellow", 
+        "office.vat": "GreenYellow", 
+        "other": "AliceBlue", 
+        "parking": "AliceBlue", 
+        "pharm": "HotPink", 
+        "recreation.books": "SandyBrown", 
+        "recreation.coffee": "Plum", 
+        "recreation.hoog": "Plum",
+        "recreation.kids.crafts": "SandyBrown", 
+        "recreation.kids.games": "SandyBrown", 
+        "recreation.other": "LightCoral", 
+        "recreation.tv": "LightCoral", 
+        "restaurants": "Orchid", 
+        "restaurants.takeout": "Orchid", 
+        "rout6": "OldLace",
+	"vacation": "Aqua" 
+} 
+  
+chrome.runtime.onInstalled.addListener(function(details){
+    if(details.reason == "install"){
+        console.log("first installation; saving dict to storage");
+        chrome.storage.local.set({'the_categories': orig_categorized_labels}, function() {
+          console.log('dict  saved');
+        });
+    }else if(details.reason == "update"){
+	console.log("update; not overriding dict; might contain personal details");
+    }
+});  
+
+
+//some on click heandler will update storage with user settings here...
+
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    
+    if (request.what == "get_dict") {
+	sendResponse({status: "got the message. working on it... will send response when done"});
+	chrome.storage.local.get("the_categories", function (obj) { 
+	  var categorized_labels = obj.the_categories;
+	  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	    chrome.tabs.sendMessage(tabs[0].id, { status: "categories and colors", 
+            the_categories: categorized_labels,the_colors: colors}, 
+            null)
+	  });
+	});
+    }
+    else {
+        sendResponse({status: "don't know what to do with that..."});
+    }
+  }
+);
+
