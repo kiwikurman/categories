dc0d998b6c70ae65ac59e528a9f6c715441011a9
mark <mark.kurman@gmail.com>
2017-11-24 23:14:35 +0200

    added selector options from background and logic that selects from selection only if input is empty

--- src/popup.js
+++ src/popup.js
@@ -8,31 +8,50 @@ document.addEventListener('DOMContentLoaded', function() {
         set_category_to_local_storage();
   });
 
-  set_selection_list();
+  set_selection_list_from_storage();
+  get_unknown_label_from_content();
+
 });
 
+//1. set selection to dict
+//2. smooth the label and set logic
+//in the set button if text is empy use selection
 
+function set_selection_list_from_storage() {
+  chrome.storage.local.get("the_categories", function (obj) {
+    var categorized_labels = obj.the_categories.values;
+    
+    //get the selector element and clear the options
+    var categories_selector = document.getElementById('category_input_selector');
+    while (categories_selector.firstChild) {
+      categories_selector.removeChild(categories_selector.firstChild);
+    }
+    
+    var unique_categories = [];
+    categorized_labels.forEach(function(element) {
+      if (unique_categories.includes(element.category) == false) {
+	unique_categories.push(element.category);
+	var opt = document.createElement('option');
+	opt.innerHTML = element.category;
+	opt.value = element.category;
+	categories_selector.appendChild(opt);
+      }
+    });
+  }); 
+}
 
-function set_selection_list() {
+function get_unknown_label_from_content() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {what: "get_name_category_map"}, function(response) {
       if (response !== undefined && response.what == "map" ) {
-	var categories_selector = document.getElementById('category_input_selector');
-	while (categories_selector.firstChild) {
-	  categories_selector.removeChild(categories_selector.firstChild);
-	}
-
-	var labels = Object.keys(response.the_name_category_map);
 	var category_map = response.the_name_category_map;
-	    console.log(category_map);
-	var unique_categories = [];
-  	for (var i in category_map) {
-	  if (unique_categories.includes(category_map[i]) == false) {
-	    unique_categories.push(category_map[i]);
-	    var opt = document.createElement('option');
-	    opt.innerHTML = category_map[i];
-	    opt.value = category_map[i];
-	    categories_selector.appendChild(opt);
+	console.log(category_map);
+	for (var key in category_map) {
+	  console.log(key);
+	  if (category_map[key] == "unknown") {
+	    var the_label = document.getElementById('category_label');
+	    the_label.innerHTML = key;
+	    break;
 	  }
 	}
       }
@@ -49,13 +68,17 @@ function set_selection_list() {
 function set_category_to_local_storage() {
   var the_label = document.getElementById('category_label').innerHTML;
   var user_input_category = document.getElementById("category_input").value;
+  var categories_selector = document.getElementById('category_input_selector');
 
+  var selection = user_input_category.length > 0 ? user_input_category : 
+    categories_selector.options[categories_selector.selectedIndex].value;
+ 
   chrome.storage.local.get("the_categories", function (obj) {
     var categorized_labels = obj.the_categories;
-    categorized_labels.values.push({label: the_label, category: user_input_category});
+    categorized_labels.values.push({label: the_label, category: selection});
     chrome.storage.local.set({'the_categories': categorized_labels}, function() {
       console.log('dict  saved');
-      document.getElementById('category_label').innerHTML = "no unknow catagories matched";
+      document.getElementById('category_label').innerHTML = "updated... realod page and check it out";
     });
   }); 
 }
